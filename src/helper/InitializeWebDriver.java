c38acb0f980fcdd74e8645812ad5341fc614a32c
Vera Djakova <vdjakova@gmail.com>
2017-10-25 21:34:45 +0300

    New test added. Project structure updates.

---
+++ src/helper/InitializeWebDriver.java
@@ -0,0 +1,27 @@
+package helper;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class InitializeWebDriver {
+
+    public static WebDriver driver = null;
+    private final static File file = new File("chromedriver.exe");
+
+    @Before
+    public void beforeScenario() {
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void afterScenario() {
+        driver.quit();
+    }
+}
